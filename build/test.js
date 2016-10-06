d7fef6c914829cccc21b6311b36de4e054f803b8
Robert Hallowell <robert.hallowell@clearcapital.com>
2016-10-06 00:37:18 -0700

    first commit, got basic build set up with babel angular and http-server for local dev

---
+++ build/test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var test = function test() {
+  return alert('yoyo');
+};
+
+test();
+
+var test2 = function test2() {
+  return console.log('poop', poop);
+};
---
+++ src/test.js
@@ -0,0 +1,5 @@
+let test = () => alert('yoyo');
+
+test();
+
+const test2 = () => console.log('poop', poop);
